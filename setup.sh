f6e78d10efefd30cb7f549e1aae46aeb81a4dd9e
Justin Bell <justin.bell@mail.mcgill.ca>
2018-04-11 16:11:17 -0400

    Switch download to processed data and add download for GloVe word embeddings

--- setup.sh
+++ setup.sh
@@ -1,13 +1,32 @@
 # abort on any error
 set -e
 
-DATAURL='https://storage.googleapis.com/comp551/cnn_dm_data.zip'
+DATAURL='https://storage.googleapis.com/comp551/processed.tar.gz'
+GLOVEURL='https://nlp.stanford.edu/data/glove.6B.zip'
 
-if [ ! -f data/train.bin ]; then
-    echo "Downloading dataset"
+if [ ! -d .env ]; then
+    echo "Creating virtualenv and downloading dependencies"
+    virtualenv .env
+    source .env/bin/activate
+    pip install -r requirements.txt
+else
+    echo "Virtualenv already created"
+fi
+
+if [ ! -d data/processed ]; then
+    echo "Downloading processed dataset"
     curl -S $DATAURL > .data.tmp.zip
-    unzip -j .data.tmp.zip -d data
+    unzip .data.tmp.zip -d data
     rm .data.tmp.zip
 else
     echo "Dataset already downloaded"
+fi
+
+if [ ! -d resources/glove.6B ]; then
+    echo "Downloading GloVe word embeddings"
+    curl -S $GLOVEURL > .glove.tmp.zip
+    unzip .glove.tmp.zip -d resources
+    rm .glove.tmp.zip
+else
+    echo "GloVe word embeddings already downloaded"
 fi
